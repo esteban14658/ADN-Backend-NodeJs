c4376b4a5e27badcb34516a58521a322180b2bb7
Rodrigo Esteban Beltran Abello - Ceiba Software <esteban.beltran@ceiba.com.co>
2023-06-26 09:04:34 -0500

    Corrijo algunos errores con el modulo de cliente

---
+++ src/infraestructura/cliente/proveedor/cliente-proveedor.module.ts
@@ -0,0 +1,31 @@
+import { Module } from "@nestjs/common";
+import { ClienteEntidad } from "../entidad/cliente.entidad";
+import { ManejadorListarCliente } from "src/aplicacion/cliente/consulta/listar-clientes.manejador";
+import { ManejadorRegistrarCliente } from "src/aplicacion/cliente/comando/registrar-cliente.manejador";
+import { daoClienteProvider } from "./dao/dao-cliente.proveedor";
+import { RepositorioCliente } from "src/dominio/cliente/puerto/repositorio/repositorio-cliente";
+import { servicioRegistrarClienteProveedor } from "./servicio/servicio-registrar-cliente.proveedor";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ServicioRegistrarCliente } from "src/dominio/cliente/servicio/servicio-registrar-cliente";
+import { DaoCliente } from "src/dominio/cliente/puerto/dao/dao-cliente";
+import { repositorioClienteProvider } from "./repositorio/repositorio-cliente.proveedor";
+
+@Module({
+    imports: [TypeOrmModule.forFeature([ClienteEntidad])],
+    providers: [
+        { provide: ServicioRegistrarCliente, inject: [RepositorioCliente], useFactory: servicioRegistrarClienteProveedor },
+        repositorioClienteProvider,
+        daoClienteProvider,
+        ManejadorRegistrarCliente,
+        ManejadorListarCliente,
+    ],
+    exports: [
+        ServicioRegistrarCliente,
+        ManejadorRegistrarCliente,
+        ManejadorListarCliente,
+        RepositorioCliente,
+        DaoCliente,
+    ],
+    })
+    export class ClienteProveedorModule { }
+    
--- src/infraestructura/cliente/proveedor/repositorio/repositorio-cliente.proveedor.ts
+++ src/infraestructura/cliente/proveedor/repositorio/repositorio-cliente.proveedor.ts
@@ -1,7 +1,7 @@
 import { RepositorioCliente } from "src/dominio/cliente/puerto/repositorio/repositorio-cliente";
 import { RepositorioClienteMysql } from "../../adaptador/repositorio/repositorio-cliente-mysql";
 
-export const repositorioUsuarioProvider = {
+export const repositorioClienteProvider = {
     provide: RepositorioCliente,
     useClass: RepositorioClienteMysql
     };

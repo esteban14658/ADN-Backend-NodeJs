f9e306861d5d7a2132045eb8a1c373ac26917cee
Rodrigo Esteban Beltran Abello - Ceiba Software <esteban.beltran@ceiba.com.co>
2023-06-30 10:31:55 -0500

    Funcionando insert de reserva

--- src/infraestructura/reserva/adaptador/repositorio/repositorio-reserva-mysql.ts
+++ src/infraestructura/reserva/adaptador/repositorio/repositorio-reserva-mysql.ts
@@ -16,37 +16,10 @@ export class RepositorioReservaMysql implements RepositorioReserva{
         @InjectEntityManager()
         private readonly entityManager: EntityManager
     ) {}
-    
-    async guardar(reserva: Reserva) {
-        /*const entidad = new ReservaEntidad();
-        entidad.fechaInicio = reserva.fechaInicio;
-        entidad.fechaFin = reserva.fechaFin;
-        console.log('[Hola]' + this.daoCliente.consultarCliente(reserva.cliente.cedula));
-        entidad.cliente.id = 1;
-        entidad.cliente.cedula = reserva.cliente.cedula;
-        entidad.cliente.nombre = reserva.cliente.nombre;
-        entidad.cliente.apellidos = reserva.cliente.apellidos;
-        entidad.cliente.telefono = reserva.cliente.telefono;
-        entidad.cliente.email = reserva.cliente.email;*/
-        const entidad = {
-            fechaInicio: reserva.fechaInicio,
-            fechaFin: reserva.fechaFin,
-            cliente: {
-                id: 1,
-                cedula: reserva.cliente.cedula,
-                nombre: reserva.cliente.nombre,
-                apellidos: reserva.cliente.apellidos,
-                telefono: reserva.cliente.telefono,
-                email: reserva.cliente.email
-            }
-        }
-        await this.repositorio.save(entidad);
-    }
 
     async guardarNativo(reserva: Reserva){
         const idCliente = (await this.daoCliente.consultarCliente(reserva.cliente.cedula)).id;
         await this.entityManager.query("INSERT INTO RESERVA (fechaInicio, fechaFin, clienteId) VALUES (?, ?, ?)", 
                                 [reserva.fechaInicio, reserva.fechaFin, idCliente]);
-        console.log(this.daoCliente.consultarCliente(reserva.cliente.cedula));
     }
 }

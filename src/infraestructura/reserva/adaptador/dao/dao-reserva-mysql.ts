e741e506250a32c62c4f3ef7bfb8ad102c97e5df
Rodrigo Esteban Beltran Abello - Ceiba Software <esteban.beltran@ceiba.com.co>
2023-07-05 11:13:41 -0500

    Estoy agregando la logica para evitar doble insersion en una reserva ya existente

--- src/infraestructura/reserva/adaptador/dao/dao-reserva-mysql.ts
+++ src/infraestructura/reserva/adaptador/dao/dao-reserva-mysql.ts
@@ -3,8 +3,6 @@ import { InjectEntityManager } from '@nestjs/typeorm';
 import { Injectable } from '@nestjs/common';
 import { ReservaDto } from 'src/aplicacion/reserva/consulta/dto/reserva.dto';
 import { DaoReserva } from 'src/dominio/reserva/puerto/dao/dao-reserva';
-import { ReservaEntidad } from '../../entidad/reserva.entidad';
-import { MoreThanOrEqual, LessThanOrEqual } from 'typeorm';
 
 @Injectable()
 export class DaoReservaMysql implements DaoReserva {
@@ -20,19 +18,14 @@ export class DaoReservaMysql implements DaoReserva {
     }
 
     async existeReserva(_fechaInicio: Date, _fechaFin: Date): Promise<Boolean> {
-        const contarFechaInicio = await this.entityManager.count(ReservaEntidad, {
-            fechaInicio: MoreThanOrEqual(_fechaInicio),
-            fechaFin: LessThanOrEqual(_fechaFin),
-        });
-        console.log('[contarFechaInicio] = ' + contarFechaInicio);
-        const contarFechaFin = await this.entityManager.count(ReservaEntidad, {
-            fechaInicio: MoreThanOrEqual(_fechaFin),
-            fechaFin: LessThanOrEqual(_fechaInicio),
-        });
-        console.log('[contarFechaFin] = ' + contarFechaFin);
-        if (contarFechaInicio > 0 || contarFechaFin > 0){
-            return true;
-        }
-        return false;
+        const resultado = await this.entityManager.query(
+            `SELECT COUNT(*)
+            FROM RESERVA
+            WHERE ? BETWEEN fechaInicio AND fechaFin
+            OR ? BETWEEN fechaInicio AND fechaFin`,
+            [_fechaInicio, _fechaFin]
+        )
+        const count = parseInt(resultado[0]['COUNT(*)']);
+        return count > 0;
     }
 }
